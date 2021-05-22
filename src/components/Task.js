96d39a3d962d852560612b4728231cb9dbc2c1e0
github username <github email address>
2021-05-22 14:25:18 -0400

    practice working with state and props in React

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({ taskObj: { text, category }, handleDelete }) {
+  function handleDeletButton() {
+    handleDelete(text);
+  }
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDeletButton}>
+        X
+      </button>
     </div>
   );
 }
